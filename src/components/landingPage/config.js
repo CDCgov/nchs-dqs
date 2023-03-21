12ad4d1d4172aa4722a21f467519a1ada018f52c
Lincoln Bollschweiler <rlv1@cdc.gov>
2023-03-21 08:35:47 -0600

    feature/18983-Updates-topic-dropdown-groupings

--- src/components/landingPage/config.js
+++ src/components/landingPage/config.js
@@ -253,25 +253,25 @@ export const topicLookup = {
 		dataUrl: "https://data.cdc.gov/NCHS/DQS-Obesity-among-children-and-adolescents-aged-2-/64sz-mcbq",
 		socrataId: "64sz-mcbq",
 		private: "1",
-		chartTitle: "Obesity among Children",
+		chartTitle: "Obesity among Children (HUS)",
 		filters: "AsianPacific,Black,Children,Hispanic,Poverty,White",
 		classificationId: 1,
 		yAxisUnitId: 1,
 		hasCI: true,
 		hasClassification: true,
-		topicGroup: 5,
+		topicGroup: 0,
 	},
 	"obesity-adult": {
 		dataUrl: "https://data.cdc.gov/NCHS/DQS-Normal-weight-overweight-and-obesity-among-adu/23va-ejrn",
 		socrataId: "23va-ejrn",
 		private: "1",
-		chartTitle: "Obesity among Adults",
+		chartTitle: "Obesity among Adults (HUS)",
 		filters: "Adults,Asian,Black,Hispanic,Poverty,White,Male,Female",
 		classificationId: 1,
 		yAxisUnitId: 1,
 		hasCI: true,
 		hasClassification: true,
-		topicGroup: 5,
+		topicGroup: 0,
 	},
 	suicide: {
 		dataUrl: "https://data.cdc.gov/NCHS/DQS-Death-rates-for-suicide-by-sex-race-Hispanic-o/u9f7-4q6s",
@@ -283,19 +283,19 @@ export const topicLookup = {
 		yAxisUnitId: 2,
 		hasCI: false,
 		hasClassification: false,
-		topicGroup: 4,
+		topicGroup: 1,
 	},
 	injury: {
 		dataUrl: "https://data.cdc.gov/NCHS/DQS-Initial-injury-related-visits-to-hospital-emer/k99r-jkp7",
 		socrataId: "k99r-jkp7",
 		private: "1",
-		chartTitle: "Initial injury-related visits to hospital emergency departments",
+		chartTitle: "Initial injury-related visits to hospital emergency departments (HUS)",
 		filters: "Adults,Older,Children,Male,Female",
 		classificationId: 1,
 		yAxisUnitId: 2,
 		hasCI: false,
 		hasClassification: false,
-		topicGroup: 10,
+		topicGroup: 1,
 	},
 	"infant-mortality": {
 		dataUrl: "https://data.cdc.gov/NCHS/DQS-Infant-mortality-rates-by-race-and-Hispanic-or/bzax-vvbx",
@@ -309,7 +309,7 @@ export const topicLookup = {
 		hasMap: true,
 		hasClassification: true,
 		binGranularity: 0.1,
-		topicGroup: 4,
+		topicGroup: 1,
 	},
 	birthweight: {
 		dataUrl: "https://data.cdc.gov/NCHS/DQS-Low-birthweight-live-births-by-race-and-Hispan/3p8z-99bn",
@@ -323,13 +323,13 @@ export const topicLookup = {
 		hasMap: true,
 		hasClassification: true,
 		binGranularity: 0.01,
-		topicGroup: 11,
+		topicGroup: 1,
 	},
 	medicaidU65: {
 		dataUrl: "https://data.cdc.gov/NCHS/DQS-Medicaid-coverage-among-persons-under-age-65-b/2g8y-scu5",
 		socrataId: "2g8y-scu5",
 		private: "1",
-		chartTitle: "Medicaid coverage among persons under age 65",
+		chartTitle: "Medicaid coverage among persons under age 65 (HUS)",
 		filters:
 			"Adults,Indian,Asian,AsianPacific,Black,Children,Female,FuncLimitStatus,InsuranceStatus,Hispanic,Male,Marital,Metropolitan,MultipleRace,Hawaiian,Poverty,Region,White",
 		classificationId: "NA",
@@ -348,32 +348,32 @@ export const topicLookup = {
 		yAxisUnitId: 1,
 		hasCI: false,
 		hasClassification: true,
-		topicGroup: 4,
+		topicGroup: 1,
 	},
 	"ambulatory-care": {
 		dataUrl: "https://data.cdc.gov/resource/tz8d-jy2e.json",
 		socrataId: "tz8d-jy2e",
 		private: "1",
-		chartTitle: "Ambulatory Care Visits",
+		chartTitle: "Ambulatory Care Visits (HUS)",
 		filters: "Adults,Black,Children,Female,Male,Older,Region,White",
 		classificationId: 1,
 		yAxisUnitId: 1,
 		hasCI: false,
 		hasClassification: true,
-		topicGroup: 10,
+		topicGroup: 1,
 	},
 	"access-care": {
 		dataUrl: "https://data.cdc.gov/resource/nt5r-ak33.json",
 		socrataId: "nt5r-ak33",
 		private: "1",
-		chartTitle: "Access to Care",
+		chartTitle: "Access to Care (HUS)",
 		filters:
 			"Adults,Indian,Asian,Black,Children,Education,Female,FuncLimitStatus,InsuranceStatus,Hispanic,Male,Metropolitan,MultipleRace,Hawaiian,Older,Poverty,Region,White",
 		classificationId: 1,
 		yAxisUnitId: 1,
 		hasCI: true,
 		hasClassification: true,
-		topicGroup: 0,
+		topicGroup: 3,
 	},
 };
 
@@ -393,23 +393,13 @@ nhisTopics.forEach((t) => {
 });
 
 export const topicGroups = [
-	"Access to care", // 0
-	"Cancer", // 1
-	"Circulatory system", // 2
-	"Coverage", // 3
-	"Death", // 4
-	"Endocrine system", // 5
-	"Health Status", // 6
-	"Mental Health", // 7
-	"Musculoskeletal system", // 8
-	"Nervous system", // 9
-	"Outpatient Visit", // 10
-	"Perinatal period", // 11
-	"Prescription Medication", // 12
-	"Respiratory system", // 13
-	"Sensory Impairments", // 14
-	"Tobacco usage", // 15
-	"Vaccination", // 16
+	"Diseases and conditions", // 0
+	"Epidemiology and health metrics", // 1
+	"Health behavior and risks", // 2
+	"Health systems", // 3
+	"Mental Health	", // 4
+	"Oral Health", // 5
+	"Prevention, control, and treatment", // 6
 ];
 
 // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
