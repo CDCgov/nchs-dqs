e8306d00275c9df5806650e238e7bf0bc750de96
Lincoln Bollschweiler <rlv1@cdc.gov>
2023-04-05 09:13:31 -0600

    QA changed tab background-color

--- src/components/landingPage/config.js
+++ src/components/landingPage/config.js
@@ -166,7 +166,7 @@ export const tabContent = `
 		</ul>
 		<!-- map wrapper -->
 		<div id="map-tab" aria-labelledby="ex-with-icons-tab-1">
-			<div class="content-wrapper" style="background-color: #ebf7f5; margin-top: 0px; padding-top: 1px">
+			<div class="content-wrapper" style="background-color: #ceece7; margin-top: 0px; padding-top: 1px">
 				<div id="mapSelectors"></div>
 				<div id="us-map-container">					
 					<div id="mapDownloadTitle"></div>
